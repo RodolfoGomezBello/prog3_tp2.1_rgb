6e708f7089de83a691f8949ddab91f6a3d0b2852
RodolfoGomezBello <yerb26@gmail.com>
2024-06-01 13:04:10 -0300

    Update sensors.js
    Ejercicio 1

--- ejercicio1/sensors.js
+++ ejercicio1/sensors.js
@@ -1,4 +1,18 @@
-class Sensor {}
+class Sensor {
+    constructor(id, name, type, value, unit, updated_at) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.value = value;
+        this.unit = unit;
+        this.updated_at = updated_at;
+    }
+
+    set updateValue(newValue) {
+        this.value = newValue;
+        this.updated_at = new Date().toISOString();
+    }
+}
 
 class SensorManager {
     constructor() {
@@ -33,7 +47,16 @@ class SensorManager {
         }
     }
 
-    async loadSensors(url) {}
+    async loadSensors(url) {
+        try {
+            const response = await fetch(url);
+            const sensorData = await response.json();
+            this.sensors = sensorData.map(sensor => new Sensor(sensor.id, sensor.name, sensor.type, sensor.value, sensor.unit, sensor.updated_at));
+            this.render();
+        } catch (error) {
+            console.error("Error al cargar los sensores:", error);
+        }
+    }
 
     render() {
         const container = document.getElementById("sensor-container");
